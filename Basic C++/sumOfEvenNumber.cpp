5604a34a2d71109b616a8021ff6939443a78a121
Rahat <105872616+Rahat-Kabir@users.noreply.github.com>
2022-09-10 21:16:28 +0600

    Add files via upload

---
+++ Basic C++/sumOfEvenNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+int sum=0;
+cout<< "Enter a number : " << endl;
+cin>>n;
+
+for(int i=0;i<=n;i++){
+    if(i%2==0){
+        sum+=i;
+    }
+}
+
+cout<<"Summation of Even number : " << sum;
+
+return 0;
+}
+
