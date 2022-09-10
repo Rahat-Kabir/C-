7783afd0a20c26ee0182e7357083fef7d48d62a7
Rahat <105872616+Rahat-Kabir@users.noreply.github.com>
2022-09-10 21:27:45 +0600

    Add files via upload

---
+++ Basic C++/sizeOfDataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << endl <<  " Find Size of fundamental data types : " << endl << endl;
+
+   cout << " The Size Of (char) is :       " << sizeof(char) << " bytes " <<endl;
+   cout << " The Size Of (short) is :    " << sizeof(short) << " bytes " <<endl ;
+   cout << " The Size Of (int) is :       " << sizeof(int) << " bytes "<<endl ;
+   cout << " The Size Of (long) is :         " << sizeof(long) << " bytes "<<endl ;
+   cout << " The Size Of (long long) is :     " << sizeof(long long) << " bytes "<<endl;
+   cout << " The Size Of (float) is :      " << sizeof(float) << " bytes "<<endl ;
+   cout << " The Size Of (double) is :      " << sizeof(double) << " bytes "<<endl;
+   cout << " The Size Of (long double) is :   " << sizeof(long double) << " bytes "<<endl;
+   cout << " The Size Of (bool) is :        " << sizeof(bool) << " bytes "<<endl;
+   return 0;
+}
+
